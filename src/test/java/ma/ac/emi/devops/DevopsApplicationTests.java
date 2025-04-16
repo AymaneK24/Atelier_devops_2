37b2b2be82b94f683087b72988ea125f4d7af0c6
Aymane Kenbouch <aymanekenbouch@gmail.com>
2025-04-16 08:10:38 +0100

    first commit

---
+++ src/test/java/ma/ac/emi/devops/DevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ac.emi.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
