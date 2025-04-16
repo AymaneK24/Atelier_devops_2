a4267d2d3bf72e0c0ddad0c9c01142fca729fec3
unknown <aymanelouahi@gmail.com>
2025-04-16 10:17:52 +0100

    first commit dev

--- src/main/java/ma/ac/emi/devops/DevopsApplication.java
+++ src/main/java/ma/ac/emi/devops/DevopsApplication.java
@@ -1,13 +1,24 @@
 package ma.ac.emi.devops;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DevopsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsApplication.class, args);
+	}
+
+	//Version 1.2 modifications
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("Hello World v1.2");
+		};
 	}
 
 }
